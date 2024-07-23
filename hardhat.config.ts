df4352da8d7841c10ce3caeaca4de02c8f785b1b
Elham Yudhistira <bugelindah22@gmail.com>
2024-07-23 16:10:40 +0000

    task 2

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -12,7 +12,7 @@ const config: HardhatUserConfig = {
   networks: {
     swisstronik: {
       url: 'https://json-rpc.testnet.swisstronik.com/',
-      accounts: [`0x${process.env.PRIVATE_KEY}`],
+      accounts: [`0xb93832a4f2119373f2eb43e8e748500511cef74330fe15c60e2f75d0ecae7fca`],
     },
   },
 }
--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,3 +1,3 @@
-const deployedAddress = ''
+const deployedAddress = '0x830f8E375E6f770C82e69be26E568fc009AB590b'
 
 export default deployedAddress
